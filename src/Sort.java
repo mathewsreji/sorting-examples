621a089f118551dc506744c15a36726cc55acc82
Reji Mathews <contactreji@gmail.com>
2021-11-25 22:18:31 -0500

    merge sort

---
+++ src/Sort.java
@@ -0,0 +1,17 @@
+import com.reji.sort.MergeSort;
+
+public class Sort {
+    public static void main(String[] args) {
+
+        // unsorted data
+        int[] data = {3,7,5,9,1,2,6,12,87,45,67,16};
+
+        MergeSort sort = new MergeSort();
+        int[] result = sort.sort(data); // invoke merge sort
+
+        // print result array
+        for(int i=0;i<result.length;i++) {
+            System.out.print(result[i] + ", ");
+        }
+    }
+}
---
+++ src/com/reji/sort/MergeSort.java
@@ -0,0 +1,51 @@
+package com.reji.sort;
+
+import java.util.Arrays;
+
+public class MergeSort {
+    /**
+     * merge sort
+     * @param data
+     * @return
+     */
+    public int[] sort(int[] data) {
+        if(data.length == 0) {
+            return data;
+        }
+        if(data.length == 1) {
+            return data;
+        } else {
+            int pivot = data.length/2;
+            int[] sorted1 = sort(Arrays.copyOfRange(data, 0, pivot));
+            int[] sorted2 = sort(Arrays.copyOfRange(data, pivot, data.length));
+            return merge(sorted1, sorted2);
+        }
+    }
+
+    private int[] merge(int[] a, int[] b){
+        int[] result = new int[a.length + b.length];
+        int i, j, k = 0;
+        for(i = 0, j = 0; k < a.length + b.length; k++) {
+
+            if(i == a.length) {  // case when array a[] has been completely merged
+                result[k] = b[j];
+                j++;
+                continue;
+            } else if (j == b.length) { // case when array b[] has been completely merged
+                result[k] = a[i];
+                i++;
+                continue;
+            } else {       // normal case
+                if (a[i] < b[j]) {
+                    result[k] = a[i];
+                    i++;
+                } else {
+                    result[k] = b[j];
+                    j++;
+                }
+            }
+        }
+        return result;
+    }
+
+}
